15b6041353bbe4c5360d7584e952795ace6f04ea
lenny <lennyjmv97@gmail.com>
2024-03-18 10:43:36 -0400

    Update added server anda database

--- src/components/layout/SwapToken.jsx
+++ src/components/layout/SwapToken.jsx
@@ -1,1 +1,34 @@
-d
+import React, { useContext, useEffect, useState } from "react";
+import DodoSwapIframe from "../web3/SwapDodo";
+import { WalletContext } from "../context/WalletContext"; // Asegúrate que la ruta es correcta para tu proyecto
+import { ThemeContext } from "../context/ThemeContext"; // Asegúrate que esta ruta es correcta para tu proyecto
+
+const App = () => {
+  const { account, network, balance } = useContext(WalletContext);
+  const { isDarkMode } = useContext(ThemeContext);
+  const [isConnected, setIsConnected] = useState(false);
+  
+  useEffect(() => {
+    if(account && network && balance !== null) {
+      setIsConnected(true);
+    } else {
+      setIsConnected(false);
+    }
+  }, [account, network, balance]);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh', textAlign: 'center' }}>
+      <h1 style={{ marginBottom: '10px', fontSize: '30px', fontWeight: 'bold',  color: isDarkMode ? 'white' : 'black' }}>Swap Widget</h1>
+      {isConnected ? 
+        (
+          <DodoSwapIframe />
+        ) : 
+        (
+          <p>Por favor, conecta tu wallet para utilizar el widget.</p>
+        )}
+    </div>
+  );
+
+};
+
+export default App;
