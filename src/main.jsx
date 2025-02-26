e140141a8edcab11d6364556cb72d2a7f3c2117f
LennyDevX <lennyjmv97@outlook.com>
2025-02-26 10:14:37 -0500

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './Styles/index.css';
-import './styles/animations.css'; // Add this line to import our custom animations
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
